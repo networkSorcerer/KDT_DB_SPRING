bc8062617246a4a5acbafddb695b5e58a38b62f2
ygkim <kelen536@gmail.com>
2024-10-28 17:08:25 +0900

    ddddd

--- hotels/src/main/java/com/kdt/hotels/dao/ReservationDAO.java
+++ hotels/src/main/java/com/kdt/hotels/dao/ReservationDAO.java
@@ -1,7 +1,27 @@
 package com.kdt.hotels.dao;
 
+import com.kdt.hotels.mapper.ReservationRowMapper;
+import com.kdt.hotels.vo.ReservationVO;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class ReservationDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    public ReservationDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<ReservationVO> userReservationList(){
+        String sql = "SELECT RE.RESERVEID, RE.USERID, RE.HOTELID, HO.HOTELNAME, " +
+                        "RE.STARTDATE, RE.ENDDATE, RE.ROOMID, RO.ROOMTYPE, RO.PRICE " +
+                        "FROM RESERVATION RE JOIN HOTEL HO " +
+                                            "ON RE.HOTELID = HO.HOTELID " +
+                                            "JOIN ROOM RO " +
+                                            "ON RE.ROOMID = RO.ROOMID";
+        return jdbcTemplate.query(sql, new ReservationRowMapper());
+    }
 }
